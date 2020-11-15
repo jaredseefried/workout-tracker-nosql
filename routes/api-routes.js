7a81976b8c3289e62cf1dce689802b43c32d70e8
Jared Seefried <jared.seefried@yahoo.com>
2020-11-15 09:15:11 -0700

    Added Schema to workout

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -2,6 +2,24 @@
 const express = require('express')
 const app = express()
 
+const db = require('../models')
+
+
+
+
+
+/*
+app.get("/api/workouts", (req, res) => {
+    db.Workout.find({}, (err, data) => {
+        if (err) {
+            console.log(err)
+        } else {
+            res.json(data)
+        }
+    })
+})
+*/
+
 // Export app
 module.exports = (app)
 
