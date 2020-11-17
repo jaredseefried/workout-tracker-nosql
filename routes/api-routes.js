938c00d33c5ee7b2af859b3c33ed7c45ecd6a9c6
Jared Seefried <jared.seefried@yahoo.com>
2020-11-16 18:54:22 -0700

    Added the POST and PUT routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -15,6 +15,29 @@ app.get("/api/workouts", (req, res) => {
     })
 })
 
+app.post("/api/workouts", (req, res) => {
+    db.Workout.create({}, (err, data) =>{
+        if (err) {
+            console.log(err)
+        } else {
+            res.json(data)
+        }
+    })
+})
+
+app.put("/api/workouts/:id", (req, res) =>{
+    db.Workout.findOneAndUpdate(
+        {_id: req.params.id},
+        { $push: { exercises: req.body}},
+        { new: true}, (err, data) =>{
+            if (err) {
+                console.log(err)
+            } else {
+                res.json(data)
+            }
+        })
+})
+
 
 // Export app
 module.exports = (app)
